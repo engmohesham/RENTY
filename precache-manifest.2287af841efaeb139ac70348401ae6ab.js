ce9ea6837be6284a80ad9c5755154788662c8b3c
Mohamed Hesham <mohamedhesham695@gmail.com>
2023-03-16 01:14:28 +0200

    Updates

---
+++ precache-manifest.2287af841efaeb139ac70348401ae6ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e0c7d5518db09c427aa60d2a52070e7",
+    "url": "/RENTY/index.html"
+  },
+  {
+    "revision": "75196ccf1c0f9fb221b7",
+    "url": "/RENTY/static/js/2.26bf003f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/RENTY/static/js/2.26bf003f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cffdd62ae7f8115698b",
+    "url": "/RENTY/static/js/main.6e800edd.chunk.js"
+  },
+  {
+    "revision": "1e30d9aba41b97fe9e61",
+    "url": "/RENTY/static/js/runtime-main.5c319202.js"
+  }
+]);
